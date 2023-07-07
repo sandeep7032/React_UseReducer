86e9cc5bed8484be38d05b769cf6175be03bff23
Sandeep <95558185+sandeep7032@users.noreply.github.com>
2023-07-07 10:54:20 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Reducer from './Components/Reducer';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Reducer />
+    </div>
+  );
+}
+
+export default App;
